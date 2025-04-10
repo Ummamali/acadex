ad8fba29f5197b6c38daf842492c8b175f04ba6e
Ummam Ali (Lappy) <sblue7446@gmail.com>
2025-04-10 18:45:16 +0500

    added the modal and started working on context

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Ledger from "./Components/Ledger/Ledger";
+import Modal from "./Components/utils/Modal";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Modal />
       <Header />
       <Ledger />
     </>
